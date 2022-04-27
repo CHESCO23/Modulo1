28bcc773e9b17408286513ea583e7f6a9bfaa10d
CHESCO23 <102255747+CHESCO23@users.noreply.github.com>
2022-04-27 17:12:14 -0300

    Add files via upload

---
+++ vetores/vetoresdedimensão.java
@@ -0,0 +1,11 @@
+package vetores;
+
+class VetoresMaisDe2Dimensoes {
+        public static void main(String[] args) {
+            Double[] faturamentoJaneiro = new Double[] { 1500.0, 2000.0, 1700.0 };
+            Double[][] faturamentoAnual = new Double[][] { faturamentoJaneiro };
+            Double[][][] faturamentoQuinquenal = new Double[][][] { faturamentoAnual };
+            Double[][][][] faturamentoDaDecada = new Double[][][][] { faturamentoQuinquenal };
+        }
+    }
+
