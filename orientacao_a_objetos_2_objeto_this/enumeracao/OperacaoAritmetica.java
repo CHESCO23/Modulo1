99648533e8c4775e8f8ae9758e3c3da54711b90e
CHESCO23 <102255747+CHESCO23@users.noreply.github.com>
2022-05-18 21:47:01 -0300

    Add files via upload

---
+++ orientacao_a_objetos_2_objeto_this/enumeracao/OperacaoAritmetica.java
@@ -0,0 +1,17 @@
+package enumeracao;
+
+public enum OperacaoAritmetica {
+    ADICAO{
+
+            public int operacao(int x, int y) {
+                return x + y;
+            }
+        }, SUBTRACAO {
+
+            public int operacao(int x, int y) {
+                return x - y;
+            }
+        };
+        public abstract int operacao(int x, int y);
+    }
+
