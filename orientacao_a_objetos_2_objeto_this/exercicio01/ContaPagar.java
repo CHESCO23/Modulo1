fd166887604bd235dcea03f43ef75490b4b4f47a
CHESCO23 <102255747+CHESCO23@users.noreply.github.com>
2022-05-23 16:11:49 -0300

    Add files via upload

---
+++ orientacao_a_objetos_2_objeto_this/exercicio01/ContaPagar.java
@@ -0,0 +1,77 @@
+package orientacao_a_objetos_2_objeto_this.exercicio01;
+
+public class ContaPagar {
+
+    String Descricao;
+    Double valor;
+    String DataVencimento;
+    Fornecedor fornecedor;
+    SituacaoConta situacaoConta;
+
+    public ContaPagar() {
+        this.situacaoConta = SituacaoConta.PENDENTE;
+
+    }
+
+    public ContaPagar(String descricao, Double valor, String dataVencimento, Fornecedor fornecedor) {
+        this();
+        this.Descricao = descricao;
+        this.valor = valor;
+        this.DataVencimento = dataVencimento;
+        this.fornecedor = fornecedor;
+
+
+    }
+
+    void pagar(){
+        //  equals é para fazer a comparação.
+        if (SituacaoConta.PAGA.equals(this.getSituacaoConta())){
+            situacaoConta = SituacaoConta.CANCELADA ; System.out.println("A conta já foi cancelada e não pode ser paga" + this.getDescricao() + ".");
+    }
+        else{}
+
+        System.out.println("pagando conta" + this.getDescricao() + " no valor de " + this.getValor()+ "e vencimento em " + this.getDataVencimento() + " do fornecedor" + this.getFornecedor().getNome() + ".");
+
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        Descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getDataVencimento() {
+        return DataVencimento;
+    }
+
+    public void setDataVencimento(String dataVencimento) {
+        DataVencimento = dataVencimento;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+
+    }
+    public SituacaoConta getSituacaoConta() {
+        return situacaoConta;
+    }
+
+    public void cancelar() {
+        System.out.println("não se pode cancelar uma conta que já foi cancelada ou paga.");
+    }
+}
+
