2df3ad23e792e82edcc7556c2707977f79376c93
CHESCO23 <102255747+CHESCO23@users.noreply.github.com>
2022-05-30 17:24:12 -0300

    Add files via upload

---
+++ MaquinaDeCafe/MaquinaCafe.java
@@ -0,0 +1,21 @@
+package orientacao_a_objetos_2_objeto_this.exercicio03;
+
+public class MaquinaCafe {
+    public Double acucarDisponivel = 200d;
+
+    public void fazerCafe(int acucar) {
+        this.acucarDisponivel = acucarDisponivel;
+        System.out.println("Está sendo preparado com "+ acucar +" gramas de açucar");
+
+    }
+    public void fazerCafe(){
+        System.out.println("Fazendo café com 10 gramas de açucar");
+
+    }
+    public void fazerCafe(){
+
+    }
+
+
+    }
+
